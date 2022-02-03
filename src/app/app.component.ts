94c9df3992197dd308c66947e1b7a045eb85d930
rajavasanthan <rajavasanthan@gmail.com>
2022-02-03 11:54:07 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstAngular';
+  username = "Person 1";
+  timer = 0;
+  isEnabel = true;
+  constructor(){
+    
+  }
+
+  enabelBtn(){
+    this.isEnabel = !this.isEnabel
+  }
+
+  incTimmer(){
+    this.timer = this.timer + 1
+  }
+
+  // Function is a block of code with name
+  // which will execute when ever it is called
+  // it can have a parameter
+  // execution can change based on the parameter
 }
